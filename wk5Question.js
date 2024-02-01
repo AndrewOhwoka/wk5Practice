47b35722ee7c602e4f3d080fd74222f6c8a20224
AndrewOhwoka <andrew.ohwoka@keyin.com>
2024-02-01 02:48:31 -0330

    FirstCommit

---
+++ wk5Question.js
@@ -0,0 +1,19 @@
+// 1. Create a function called greet that takes a name as an argument and returns a greeting message with a sign of exclamation at the end of the name.For example: greet(“Alan”) returns “Welcome Alan!”.Call the function with different names to test it.
+
+// 2. Modify the above greet function to have default parameter for name, so it provides a generic greeting if no name is provided. [You may want to write more shapes of greet function using other techniques to handle missing arguments (as discussed in class)]
+
+// 3. Write a function called calculateArea that takes the radius of a circle as a parameter and returns the area. You might want to use Math object with PI value (like Math.PI)For example: calculateArea(5) returns the area by putting 5 as radius value.
+
+// 4. Declare a variable called multiply and assign an anonymous function that multiplies two numbers. Use this variable to perform multiplication.
+
+// 5. Declare a variable called Average and assign a function allowing any number of arguments (numbers), returning their average: average(10, 20, 30) should return 20.[you are required to use the “rest” “…” operator here]
+
+// 6. Write a function that returns the number of matches found for the first argument in the remaining arguments: findMatches(66, 1, 345, 2334, 66, 67, 66) should return 2. [Hint: you are required to use “rest” “…” operator here too and the technique discussed in our class on January 31,2024)
+
+// 7. Write a function that takes any number of arguments and checks the first argument, if it is a “1” It returns the sum of rest of the numbers and if it is a “0” then it returns average of the rest of the numbers.
+
+// 8. Write a function taking any number of arguments (Numbers), returning true if they are all less than 50: isUnder50(1, 2, 3, 5, 4, 65) should return false and isUnder50(1,2,3,4,5) should return false.
+
+// 9. Write a function that takes one string parameter and returns the integer number from it if it is there. If the string does not contain a number in the beginning it returns false.
+
+// 10. Write function isPositive( ) that accepts a number and returns “Positive” if it is greater than 0 and returns “Negative” if it is less than 0, and returns “Zero” if the number is 0.
